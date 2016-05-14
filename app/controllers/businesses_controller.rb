f300362b45ab942b484fcce88577a71725de0a54
Julia Dahm <jukabrown@gmail.com>
2016-05-13 21:19:28 -0400

    Database populated w/ businesses' data. Table setup in view.

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,4 +1,10 @@
 class BusinessesController < ApplicationController
+
   def index
+    @businesses = Business.all
   end
+
+  def show 
+    @business = Business.find(params[:id])
+  end 
 end
---
+++ db/migrate/20160513211742_create_businesses.rb
@@ -0,0 +1,13 @@
+class CreateBusinesses < ActiveRecord::Migration
+  def change
+    create_table :businesses do |t|
+      t.string :business_phone
+      t.string :business_type
+      t.string :name
+      t.string :owner
+      t.string :start_date
+
+      t.timestamps null: false
+    end
+  end
+end
