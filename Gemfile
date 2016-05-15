b5cb42c5a7bb89c249b6a96da259b8f733ee9041
Julia Dahm <jukabrown@gmail.com>
2016-05-14 21:24:25 -0400

    added figaro and puma

--- Gemfile
+++ Gemfile
@@ -25,6 +25,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'materialize-sass'
 gem 'will_paginate'
 gem 'filterrific'
+gem 'figaro'
+gem 'puma'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -9,12 +9,3 @@ class BusinessesController < ApplicationController
   end 
 end
 
-json.each do |business_json|
-  business = Business.new
-  business.business_phone = business_json["business_phone"]
-  business.business_type = business_json["business_type"]
-  business.name = business_json["name"]
-  business.owner = business_json["owner"]
-  business.start_date = business_json["start_date"]
-  business.save
-end
